d38de0e7ddf262a4a58613ec533001799ca5747b
Phil Marcoccia <36013742+frostie@users.noreply.github.com>
2019-10-17 13:40:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-$(document).ready(function() {
+$(document).ready(function(){
+  // Add smooth scrolling to all links
   $("a").on('click', function(event) {
 
+    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
+      // Prevent default anchor click behavior
       event.preventDefault();
 
+      // Store hash
       var hash = this.hash;
 
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
 
+        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    }
+    } // End if
   });
 });
 
-// Test: Popup msg when user hovers over Project 1
+// Popup msg when user hovers over Project 1
 var project = document.querySelector("#project1");
 
 project.addEventListener("mouseenter", mouseEnterContainer);
 
 function mouseEnterContainer() {
-  alert("I've been triggered!");
+  alert("O no! I've been triggered!");
 }
 
-// TBD: Write function for the back-to-top smooth scroll
+// TBD: Write JS here for the back-to-top smooth scroll
+
+mybutton = document.getElementById("bttbtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
