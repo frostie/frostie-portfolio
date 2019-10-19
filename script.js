09c7ff7a97df2d3b594cb72e298145a9fa4c9174
Phil Marcoccia <36013742+frostie@users.noreply.github.com>
2019-10-18 20:16:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -50,8 +50,10 @@ function scrollFunction() {
   }
 }
 
+/* Currently not using this, as I ended up linking #bttbtn to href="#title" and to smooth scrolling (above):
+
 // When the user clicks on the button, scroll to the top of the document
 function scrollToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+} */
